a3d6a6374a32d10596c81cd71c621555dd110cfd
davidelettieri <davidelettieri@users.noreply.github.com>
2021-01-03 16:15:15 +0100

    update readme

--- Kaleidoscope/IREmitter.cs
+++ Kaleidoscope/IREmitter.cs
@@ -156,25 +156,18 @@ namespace Kaleidoscope
             var _cond = expr.Condition;
             var _then = expr.Then;
             var _else = expr.Else;
-
             var (_, cond) = Visit(ctx, _cond);
-
             var zero = LLVM.ConstReal(LLVM.DoubleType(), 0);
             var cond_val = LLVM.BuildFCmp(_builder, LLVMRealPredicate.LLVMRealONE, cond, zero, "ifcond");
-
             var startBB = LLVM.GetInsertBlock(_builder);
             var the_function = LLVM.GetBasicBlockParent(startBB);
-
             var then_bb = LLVM.AppendBasicBlock(the_function, "then");
             var else_bb = LLVM.AppendBasicBlock(the_function, "else");
             var merge_bb = LLVM.AppendBasicBlock(the_function, "ifcont");
             LLVM.BuildCondBr(_builder, cond_val, then_bb, else_bb);
             LLVM.PositionBuilderAtEnd(_builder, then_bb);
             var (_, then_val) = Visit(ctx, _then);
-
             then_bb = LLVM.GetInsertBlock(_builder);
-
-
             LLVM.PositionBuilderAtEnd(_builder, else_bb);
             var (_, else_val) = Visit(ctx, _else);
             else_bb = LLVM.GetInsertBlock(_builder);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Kaleidoscope
+
+The Kaleidoscope tutorial implemented with C# and [LLVMSharp](https://github.com/microsoft/LLVMSharp).
+
+The scanner and the parser implementations do not follow the official Kaleidoscope tutorial. For what concerns the IR emitter I tried to follow more closely the official Kaleidoscope tutorial (OCaml) and the examples available in [LLVMSharp](https://github.com/microsoft/LLVMSharp) and in [here](https://ice1000.org/llvm-cs/en/).
+
+The aim of the project was to try to use LLVMSharp and to have something running wih LLVM. The behavior of the demo is not the same as the official tutorial (which I did not run) nor as the C# examples in LLVMSharp, which I was not able to make it run correctly.
+
+Feel free to drop me a line or to open an issue. Be kind it's a work in progress :)
