fbabc631126b9dba665aaa1da3ca3421546b8f0e
Davide Lettieri <davide.lettieri@gmail.com>
2025-09-28 14:12:46 +0200

    feat: scaffold tests projects

---
+++ tests/Kaleidoscope.Chapter6.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Kaleidoscope.Chapter6.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Kaleidoscope.Chapter7.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Kaleidoscope.Chapter7.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
