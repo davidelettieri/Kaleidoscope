5f4bf50da6bf70fd8817c7d0878e29159c67a6b0
Davide Lettieri <davide.lettieri@gmail.com>
2025-09-28 15:24:23 +0200

    feat: scaffold tests projects (#7)
    * feat: scaffold tests projects
    
    * feat: add tests
    
    * feat: add github actions
    
    * feat: add additional test cases
    
    * fix: newline issue
    
    * feat: cancel ci in progress
    
    * fix: newline in other project
    
    * review: share test code

---
+++ tests/Kaleidoscope.Chapter6.Tests/InterpreterTests.cs
@@ -0,0 +1,35 @@
+﻿using Kaleidoscope.Shared;
+using Kaleidoscope.Shared.AST;
+using Kaleidoscope.Shared.Tests;
+
+namespace Kaleidoscope.Chapter6.Tests;
+
+public class InterpreterTests : InterpreterTestBase
+{
+    private readonly Interpreter _sut = new();
+
+
+    [Theory]
+    [InlineData("1+5;", "6")]
+    [InlineData("1-5;", "-4")]
+    [InlineData("10*2;", "20")]
+    [InlineData("10<2;", "0")]
+    [InlineData("2 + 3 * 4;", "14")]
+    [InlineData("2 * (3 + 4);", "14")]
+    [InlineData("def unary!(v) if v then 0 else 1;!10;", "0")]
+    [InlineData("if 1 then 2 else 3;", "2")]
+    [InlineData("if 0 then 2 else 3;", "3")]
+    [InlineData("def foo(x, y) x + y * 2; foo(40, 1);", "42")]
+    [InlineData("def bar(a, b) a * b; bar(6, 8);", "48")]
+    [InlineData("def baz() 34 * 2; baz();", "68")]
+    [InlineData("def fib(n) if n < 2 then n else fib(n-1) + fib(n-2); fib(6);", "8")]
+    [InlineData("def answer() 50; answer();", "50")]
+    public void ExecutingASTProduceExpectedOutput(string source, string expected)
+    {
+        var ast = Parse(source);
+
+        _sut.Run(ast);
+
+        AssertSingleLineOutput(expected);
+    }
+}
