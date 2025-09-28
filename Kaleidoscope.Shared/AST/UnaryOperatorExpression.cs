ac5cb8006cac4e86746d4dbfba15cf9aeb7fc1cb
Davide Lettieri <davide.lettieri@gmail.com>
2025-09-28 10:56:38 +0200

    feat: style config

--- Kaleidoscope.Shared/AST/UnaryOperatorExpression.cs
+++ Kaleidoscope.Shared/AST/UnaryOperatorExpression.cs
@@ -2,7 +2,7 @@ namespace Kaleidoscope.Shared.AST;
 
 public sealed class UnaryOperatorExpression(string name, List<string> args) : PrototypeExpression("unary_" + name, args)
 {
-    public string Argument { get => Arguments[0]; }
+    public string Argument => Arguments[0];
     public override ExpressionType NodeType { get; } = ExpressionType.UnaryOperator;
 
     public override TResult Accept<TResult, TContext>(IExpressionVisitor<TResult, TContext> visitor, TContext ctx) => visitor.VisitPrototype(ctx, this);
--- Kaleidoscope.Shared/Context.cs
+++ Kaleidoscope.Shared/Context.cs
@@ -7,15 +7,9 @@ public sealed class Context
 {
     private readonly ImmutableDictionary<string, LLVMValueRef> _source;
 
-    public Context()
-    {
-        _source = ImmutableDictionary<string, LLVMValueRef>.Empty;
-    }
+    public Context() => _source = ImmutableDictionary<string, LLVMValueRef>.Empty;
 
-    private Context(ImmutableDictionary<string, LLVMValueRef> source)
-    {
-        _source = source;
-    }
+    private Context(ImmutableDictionary<string, LLVMValueRef> source) => _source = source;
 
     public Context Add(string key, LLVMValueRef value)
         => new Context(_source.SetItem(key, value));
